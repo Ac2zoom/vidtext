da4c4de5fcd97dc68ea86ce93b333ed94c0bbaf2
Akshay Chalana <ac2zoom@yahoo.com>
2019-07-13 23:33:16 -0700

    Final commit before removing from Vidify

--- vidtext/textTovideo.py
+++ vidtext/textTovideo.py
@@ -11,7 +11,6 @@ from .imagesource import getImage
 def TextToVideo(sourceData, vs=0):
 	i = 'A'
 	color = 'white'
-	# TODO: Select font based on Kostya's work
 	font = "calibri.ttf"
 	backcolor = ['#210f05', '#444441', '#16082d', '#3d091f', '#420000', '#012824', '#332401', '#250130', '#011d30']
 	li = Text(sourceData)
@@ -19,7 +18,6 @@ def TextToVideo(sourceData, vs=0):
 	dir_path = TempDir()
 	img_array = []
 	for text in range(0, len(li)):
-		# TODO: Instead use single overlaid image created by Kostya
 		img = ImageText((800, 600), background=secure_random.choice(backcolor))
 		img.write_text_box((50, 50), li[text], box_width=700, font_filename=font, font_size=30, color=color, place='justify')
 		img.save(dir_path+i+'.png')
@@ -34,7 +32,6 @@ def TextToVideo(sourceData, vs=0):
 		size = (width, height)
 		img_array.append(video_img)
 		i = ''.join(random.choices(string.ascii_uppercase + string.digits))
-	# TODO: Figure out how to append audio from Jasper's work
 	out = cv2.VideoWriter('project.mp4', cv2.VideoWriter_fourcc(*'mp4v'), vs, size)
 	for i in range(len(img_array)):
 		out.write(img_array[i])
